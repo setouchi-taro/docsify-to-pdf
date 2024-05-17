b5aad438205b5139e8383c42c84c5526435f8755
矢野 貴大 <yano@setouchisystem.co.jp>
2024-05-17 13:52:49 +0900

    PDF By File

--- src/index.js
+++ src/index.js
@@ -4,49 +4,46 @@ const logger = require("./logger.js");
 const getFreePorts = require("./free-port.js");
 
 const defaultConfig = {
-  pathToStatic: "static",
-  mainMdFilename: "main.md",
   removeTemp: true,
-  contents: "docs/_sidebar.md",
-  pathToPublic: "./pdf/readme.pdf",
-  pdfOptions: { format: "A4" },
-  emulateMedia: "print",
-  pathToDocsifyEntryPoint: ".",
+  emulateMedia: "screen",
 };
 
-const run = async incomingConfig => {
-  const [docsifyRendererPort, docsifyLiveReloadPort] = await getFreePorts();
-  const preBuildedConfig = merge(defaultConfig, incomingConfig);
+const run = async incomingConfigs => {
+  for (let i = 0 ; i < incomingConfigs.length ; i++ ) {
+    const incomingConfig = incomingConfigs[i]
+    const [docsifyRendererPort, docsifyLiveReloadPort] = await getFreePorts();
+    const preBuildedConfig = merge(defaultConfig, incomingConfig);
 
-  logger.info("Build with settings:");
-  console.log(JSON.stringify(preBuildedConfig, null, 2));
-  console.log("\n");
+    //logger.info("Build with settings:");
+    //console.log(JSON.stringify(preBuildedConfig, null, 2));
+    //console.log("\n");
 
-  const config = merge(preBuildedConfig, {
-    docsifyRendererPort,
-    docsifyLiveReloadPort,
-  });
+    const config = merge(preBuildedConfig, {
+      docsifyRendererPort,
+      docsifyLiveReloadPort,
+    });
 
-  const { combineMarkdowns } = require("./markdown-combine.js")(config);
-  const { closeProcess, prepareEnv, cleanUp } = require("./utils.js")(config);
-  const { createRoadMap } = require("./contents-builder.js")(config);
-  const { runDocsifyRenderer } = require("./docsify-server.js")(config);
-  const { htmlToPdf } = require("./render.js")(config);
+    const { combineMarkdowns } = require("./markdown-combine.js")(config);
+    const { closeProcess, prepareEnv, cleanUp } = require("./utils.js")(config);
+    const { createRoadMap } = require("./contents-builder.js")(config);
+    const { runDocsifyRenderer } = require("./docsify-server.js")(config);
+    const { htmlToPdf } = require("./render.js")(config);
 
-  try {
-    await cleanUp();
-    await prepareEnv();
-    const roadMap = await createRoadMap();
-    await combineMarkdowns(roadMap);
+    try {
+      await cleanUp();
+      await prepareEnv();
+      const roadMap = await createRoadMap();
+      await combineMarkdowns(roadMap);
 
-    runDocsifyRenderer();
-    await htmlToPdf();
+      runDocsifyRenderer();
+      await htmlToPdf();
 
-    logger.success(path.resolve(config.pathToPublic));
-  } catch (error) {
-    logger.err("run error", error);
-  } finally {
-    closeProcess(0);
+      logger.success(path.resolve(config.pathToPublic));
+    } catch (error) {
+      logger.err("run error", error);
+    } finally {
+      if (i === incomingConfigs.length -1 ) closeProcess(0);
+    }
   }
 };
 
